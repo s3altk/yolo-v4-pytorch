8e5f8e862a8c3d259577a7722d59491f73d5449d
Semen Tkachev <tka4off.96@yandex.ru>
2021-05-26 14:17:46 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -68,7 +68,7 @@ class Yolo_dataset(Dataset):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
             oh, ow, oc = img.shape
-            dh, dw = np.array(np.array([oh, ow, oc]) * self.cfg.jitter, dtype=np.int)
+            dh, dw, dc = np.array(np.array([oh, ow, oc]) * self.cfg.jitter, dtype=np.int)
 
             dhue = rand_uniform_strong(-self.cfg.hue, self.cfg.hue)
             dsat = rand_scale(self.cfg.saturation)
