b532351cc20037fe2606530764bde2e4761a0bf1
Semen Tkachev <tka4off.96@gmail.com>
2022-04-04 23:51:13 +0700

    Update coco_eval.py

--- tool/coco_eval.py
+++ tool/coco_eval.py
@@ -12,7 +12,6 @@ from pycocotools.cocoeval import COCOeval
 from pycocotools.coco import COCO
 
 from collections import defaultdict
-from . import utils
 
 class CocoEvaluator(object):
     def __init__(self, coco_gt, iou_types, bbox_fmt='coco'):
@@ -122,8 +121,8 @@ def create_common_coco_eval(coco_eval, img_ids, eval_imgs):
     coco_eval._paramsEval = copy.deepcopy(coco_eval.params)
 
 def merge(img_ids, eval_imgs):
-    all_img_ids = utils.all_gather(img_ids)
-    all_eval_imgs = utils.all_gather(eval_imgs)
+    all_img_ids = all_gather(img_ids)
+    all_eval_imgs = all_gather(eval_imgs)
 
     merged_img_ids = []
     for p in all_img_ids:
@@ -141,6 +140,35 @@ def merge(img_ids, eval_imgs):
 
     return merged_img_ids, merged_eval_imgs
 
+def all_gather(data):
+    world_size = get_world_size()
+    if world_size == 1:
+        return [data]
+
+    buffer = pickle.dumps(data)
+    storage = torch.ByteStorage.from_buffer(buffer)
+    tensor = torch.ByteTensor(storage).to("cuda")
+
+    local_size = torch.tensor([tensor.numel()], device="cuda")
+    size_list = [torch.tensor([0], device="cuda") for _ in range(world_size)]
+    dist.all_gather(size_list, local_size)
+    size_list = [int(size.item()) for size in size_list]
+    max_size = max(size_list)
+
+    tensor_list = []
+    for _ in size_list:
+        tensor_list.append(torch.empty((max_size,), dtype=torch.uint8, device="cuda"))
+    if local_size != max_size:
+        padding = torch.empty(size=(max_size - local_size,), dtype=torch.uint8, device="cuda")
+        tensor = torch.cat((tensor, padding), dim=0)
+    dist.all_gather(tensor_list, tensor)
+
+    data_list = []
+    for size, tensor in zip(size_list, tensor_list):
+        buffer = tensor.cpu().numpy().tobytes()[:size]
+        data_list.append(pickle.loads(buffer))
+
+    return data_list
 
 def loadRes(self, resFile):
     res = COCO()
